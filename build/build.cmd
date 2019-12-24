4106540eddac24f8c2f1671969ffe2c454276665
SchreinerK <ks@ksware.de>
2019-12-24 08:27:01 +0100

    DocumentationFile, tests, TargetFrameworks: net45;netcoreapp3.0

--- build/build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd..
-path=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin;%path%
-path="C:\Users\KayS\Downloads\nuget.exe";%path%
-rd /S /Q src\KsWare.Presentation.Interfaces\bin
-nuget restore src\KsWare.Presentation.Interfaces.sln
-msbuild "src\KsWare.Presentation.Interfaces.sln" /verbosity:normal /p:GeneratePackageOnBuild=false -t:Rebuild -p:Configuration=Release
-dotnet pack "src\KsWare.Presentation.Interfaces\KsWare.Presentation.Interfaces.csproj" --configuration Release --output "build\Temp" --no-build
-pause
--- src/KsWare.Presentation.Interfaces/Plugins/TemplateConverter/ITemplateConverterPlugin.cs
+++ src/KsWare.Presentation.Interfaces/Plugins/TemplateConverter/ITemplateConverterPlugin.cs
@@ -1,4 +1,7 @@
-﻿namespace KsWare.Presentation.Interfaces.Plugins.TemplateConverter
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace KsWare.Presentation.Interfaces.Plugins.TemplateConverter
 {
 	/// <summary>
 	/// Interface ITemplateConverterPlugin
@@ -9,7 +12,14 @@
 		/// Creates the data template.
 		/// </summary>
 		/// <param name="content">The content for the template.</param>
-		/// <returns><see cref="DataTemplate"/><see cref="ControlTemplate"/></returns>
-		object CreateTemplate(object content);
+		/// <returns><see cref="DataTemplate"/></returns>
+		DataTemplate CreateDataTemplate(object content);
+
+		/// <summary>
+		/// Creates the control template.
+		/// </summary>
+		/// <param name="content">The content for the template.</param>
+		/// <returns><see cref="ControlTemplate"/></returns>
+		ControlTemplate CreateControlTemplate(object content);
 	}
 }
