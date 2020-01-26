d127f2838f731eb5a5d45a5205ecf3f554569fc5
SchreinerK <ks@ksware.de>
2020-01-26 09:13:57 +0100

    .

--- src/KsWare.Presentation.Interfaces/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.Interfaces/Properties/AssemblyInfo.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System.Reflection;
+
+// namespace must equal to assembly name
+// ReSharper disable once CheckNamespace
+namespace KsWare.Presentation.Interfaces {
+
+	/// <summary>
+	/// Provides assembly information
+	/// </summary>
+	public static class AssemblyInfo {
+
+		/// <summary>
+		/// Gets this assembly.
+		/// </summary>
+		/// <value>The assembly.</value>
+		public static Assembly Assembly => Assembly.GetExecutingAssembly();
+
+		/// <summary>
+		/// The XML namespace for this assembly
+		/// </summary>
+		public const string XmlNamespace = "http://ksware.de/Presentation/Interfaces";
+
+		/// <summary>
+		/// The root namespace for this assembly
+		/// </summary>
+		public const string RootNamespace = "KsWare.Presentation.Interfaces";
+
+	}
+}
